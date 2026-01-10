48e6c3c0e5e9b893921255cf57fa4fc476941b27
Sanjay <sayraj1771@gmail.com>
2026-01-10 21:23:05 +0530

    cleanup ToolList

--- frontend/app/components/landing_page/ToolList.tsx
+++ frontend/app/components/landing_page/ToolList.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import ToolCard from "../ToolCard";
 import {
   FaCompress,
-  FaRegPenToSquare,
   FaRegTrashCan,
   FaImages,
   FaFilePdf,
   FaSort,
-  FaSignature,
   FaLock,
   FaUnlock,
   FaCodeMerge,
@@ -17,46 +15,10 @@ import {
 const ToolList: React.FC = () => {
   const tools = [
     {
-      icon: FaRegPenToSquare,
-      title: "PDF Editor",
-      description: `Edit PDF files for free. Fill & sign PDF.\nAdd text, links, images and shapes.\n Edit existing PDF text. Annotate PDF`,
-      route: "/edit-pdf",
-    },
-    {
-      icon: FaImages,
-      title: "PDF to JPG",
-      description: "Convert PDF pages to JPG files",
-      route: "/pdf-to-jpg",
-    },
-    {
-      icon: FaCompress,
-      title: "Compress",
-      description: "Reduce the size of your PDF",
-      route: "/compress-pdf",
-    },
-    {
       icon: FaSort,
       title: "Rearrange PDF Pages",
       description: "Easily drag and drop pages to reorder your PDF.",
       route: "/rearrange-pdf-pages",
-    },
-    {
-      icon: FaImages,
-      title: "PDF to PNG",
-      description: "Convert PDF pages to PNG files",
-      route: "/pdf-to-png",
-    },
-    {
-      icon: FaRegTrashCan,
-      title: "Delete Pages",
-      description: "Remove pages from a PDF document",
-      route: "/delete-pdf-pages",
-    },
-    {
-      icon: FaFilePdf,
-      title: "WORD to PDF",
-      description: "Convert WORD file to PDF with ease",
-      route: "/word-to-pdf",
     },
     {
       icon: FaFilePdf,
@@ -65,10 +27,28 @@ const ToolList: React.FC = () => {
       route: "/img-to-pdf",
     },
     {
+      icon: FaRegTrashCan,
+      title: "Delete Pages",
+      description: "Remove pages from a PDF document",
+      route: "/delete-pdf-pages",
+    },
+    {
       icon: FaScissors,
       title: "Split PDF",
       description: "Extract PDF pages or split into several single pages",
       route: "/split-pdf",
+    },
+    {
+      icon: FaImages,
+      title: "PDF to Image",
+      description: "Convert PDF pages to images",
+      route: "/pdf-to-image",
+    },
+    {
+      icon: FaCompress,
+      title: "Compress",
+      description: "Reduce the size of your PDF",
+      route: "/compress-pdf",
     },
     {
       icon: FaCodeMerge,
@@ -77,34 +57,16 @@ const ToolList: React.FC = () => {
       route: "/merge-pdf",
     },
     {
-      icon: FaFilePdf,
-      title: "PNG to PDF",
-      description: "Convert one or more PNG files to PDF",
-      route: "/png-to-pdf",
-    },
-    {
-      icon: FaLock,
-      title: "Protect PDF",
-      description: "Encrypt your PDF with a password",
-      route: "/protect-pdf",
-    },
-    {
-      icon: FaSignature,
-      title: "Fill & Sign",
-      description: "Add signature to PDF. Fill out PDF forms",
-      route: "/fill-sign-pdf",
-    },
-    {
       icon: FaUnlock,
       title: "Unlock PDF",
       description: "Remove restrictions and passowrd \n from a PDF document",
       route: "/unlock-pdf",
     },
     {
-      icon: FaImages,
-      title: "PDF to Image",
-      description: "Convert PDF pages to images",
-      route: "/pdf-to-image",
+      icon: FaLock,
+      title: "Protect PDF",
+      description: "Encrypt your PDF with a password",
+      route: "/protect-pdf",
     },
   ];
 
